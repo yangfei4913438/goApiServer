75a2b4e86b69df7e669cf5a6a48b0884758fcc67
yangfei <yf170481206>
2018-12-30 16:42:13 +0800

    注释优化

--- tools/t_os.go
+++ tools/t_os.go
@@ -17,7 +17,7 @@ func GetRootPath() string {
 	return FileDir + "/"
 }
 
-// 解析JSON文件
+// 解析JSON文件: json文件的数据结构 和 接口对象的数据结构 必须要一致！！！
 func ParseJsonFile(filename string, v interface{}) {
 	//ReadFile函数会读取文件的全部内容，并将结果以[]byte类型返回
 	data, err := ioutil.ReadFile(filename)
