57ff58afb4ff933453780e378631edae1658e474
YangFei <yangfei86@vip.qq.com>
2019-01-08 09:46:48 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -11,4 +11,4 @@ CREATE TABLE `test.users` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `user_name` (`name`) USING BTREE,
   KEY `user_age` (`age`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
