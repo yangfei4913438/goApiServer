a72f974770b0cd8e406b91adad2b6eebebb31fd6
yangfei <yangfei86@vip.qq.com>
2019-05-09 11:04:46 +0800

    - 增加一些 websocket 代码注释，便于理解。 - 优化 websocket 的中断处理，避免 beego 报错不相关的内容。

--- controllers/c_public.go
+++ controllers/c_public.go
@@ -36,8 +36,7 @@ var (
 )
 
 func (c *WebSocketController) SayHi() {
-	beego.Notice("收到请求。。。")
-
+	// 定义变量
 	var (
 		wsConn *websocket.Conn
 		err    error
@@ -46,34 +45,38 @@ func (c *WebSocketController) SayHi() {
 		data []byte
 	)
 
-	// 完成http应答
+	// 完成http应答，升级为 websocket 连接
 	if wsConn, err = upgrader.Upgrade(c.Ctx.ResponseWriter, c.Ctx.Request, nil); err != nil {
 		return // 获取连接失败直接返回
 	}
 
+	// 开始接收处理 websocket 请求
 	if conn, err = ws.InitConnection(wsConn); err != nil {
 		goto ERR
 	}
 
+	// 心跳
 	go func() {
 		var (
 			err error
 		)
 		for {
-			// 每隔一秒发送一次心跳
+			// 每隔一秒发送一次心跳，内容要转化为字节码，不能直接使用字符串。
 			if err = conn.WriteMessage([]byte("heartbeat")); err != nil {
 				return
 			}
 			time.Sleep(1 * time.Second)
 		}
-
 	}()
 
 	for {
+		// 调用方法查看 channel 已经接收的数据
 		if data, err = conn.ReadMessage(); err != nil {
 			goto ERR
 		}
 		beego.Debug("收到一条消息:", string(data))
+
+		// 发送数据，处理协程收到 channel 消息后自动发出。
 		if err = conn.WriteMessage(data); err != nil {
 			goto ERR
 		}
@@ -82,4 +85,6 @@ func (c *WebSocketController) SayHi() {
 ERR:
 	// 关闭连接
 	conn.Close()
+	// 随便返回一条信息，否则 beego 会报找不到html模板的错误!
+	c.Ctx.WriteString("")
 }
